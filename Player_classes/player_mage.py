55e0ad47ff8e159776c9719b5b24ada3fe7f35c4
Patzyh <tjark.lefhalm@gmail.com>
2024-06-13 19:34:40 +0200

    Player Klassen

---
+++ Player_classes/player_mage.py
@@ -0,0 +1,28 @@
+class Mage:
+    def __init__(self, name, lvl, hp, atk, df):
+        self.name = name
+        self.level = lvl
+        self.health = hp
+        self.attack = atk
+        self.defence = df
+
+    def get_name(self):
+        return self.name
+
+    def get_hp(self):
+        return self.health
+
+    def get_atk(self):
+        return self.attack
+
+    def get_def(self):
+        return self.defence
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_hp(self, hp):
+        self.health = hp
+
+    def set_atk(self, atk):
+        self.attack = atk
