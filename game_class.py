26f825212478c984482b5e90ae541bef894dadb5
Patzyh <tjark.lefhalm@gmail.com>
2024-06-14 17:35:10 +0200

    Spiel Klasse neue Methoden

--- game_class.py
+++ game_class.py
@@ -1,11 +1,14 @@
 from player_class import Spieler
 from enemy_class import Gegner
+import random
+
 
 
 class Spiel:
     def __init__(self, name, class_type):
         self.__spieler = Spieler(name, class_type)
         self.__punkte = 0
+        self.__rounds = 0
 
     def spielstand(self):
         print("Punkte: ", self.__punkte)
@@ -28,6 +31,31 @@ class Spiel:
 
     def get_player(self):
         return self.__spieler
+
+
+    def stealth_attack(self):
+        self.__spieler.set_hp(self.__spieler.get_hp() - self.__gegner.get_atk())
+        if self.__spieler.get_hp() <= 0:
+            print("Spieler besiegt")
+            self.stop()
+    def laufen(self):
+        self.__rounds += 1
+        self.start_next_round()
+
+    def start_next_round(self):
+        standard_gegner = random.randint(1, 5)
+        stealth = random.randint(1, 20)
+        if standard_gegner == 1:
+            self.initate_fight()
+            print("Ein Gegner ist in der Nähe. Ich sollte mich auf einen Kampf vorbereiten.")
+        else:
+            print("Es scheint kein Gegner in der Nähe zu sein. Ich kann wohl unbesorgt weitergehen.")
+        if stealth == 1:
+            print("Ein Gegener greift aus dem Schatten an.")
+            self.initate_fight()
+            self.stealth_attack()
+
+
 
 
     def spielende(self):
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
     window = ui.App("v0.0.1 INDEV")
     window.mainloop()
 
+
 print(__name__)
 
 if __name__ == "__main__":
