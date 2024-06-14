b8050203832839013445cc1c23900884ac823478
Crafxi <crafxi@gmail.com>
2024-06-14 10:22:12 +0200

    langsam wirds zu kompliziert

--- enemy_class.py
+++ enemy_class.py
@@ -1,2 +1,34 @@
+import random
+
 class Gegner:
-    pass
+    def __init__(self):
+        self.__leben = 100
+        self.__enemytype = random.randint(1,3)
+        self.__dead = False
+
+        self.__enemy = [
+            {"name": "Orc", "angriff": 14, "leben": 17},
+            {"name": "Goblin", "angriff": 16, "leben": 15},
+            {"name": "Geist", "angriff": 18, "leben": 13}
+        ]
+
+        self.__enemy = self.enemy[self.enemytype - 1]
+        self.__name = self.__enemy["name"]
+
+    def get_dead(self):
+        return self.__dead
+
+    def get_leben(self):
+        return self.__leben
+
+    def get_name(self):
+        return self.__name
+
+    def set_dead(self):
+        self.__dead = True
+
+    def set_leben(self, leben):
+        self.__leben = leben
+
+    def set_name(self, name):
+        self.__name = name
--- game_class.py
+++ game_class.py
@@ -1,30 +1,38 @@
-from time import time as Zeit
 from player_class import Spieler
 from enemy_class import Gegner
 
+
 class Spiel:
     def __init__(self):
-        self.spieler = Spieler()
-        self.gegner = Gegner()
-        self.zeit = Zeit()
-        self.zeit.start()
-        self.punkte = 0
+        self.__spieler = Spieler()
+        self.__punkte = 0
 
     def spielstand(self):
-        print("Punkte: ", self.punkte)
-        print("Zeit: ", self.zeit.get_time())
+        print("Punkte: ", self.__punkte)
+
+    def initate_fight(self):
+        self.__gegner = Gegner()
+
+    def attack(self):
+        self.__gegner.set_leben(self.__gegner.get_leben() - self.__spieler.get_atk())
+        if self.__gegner.get_leben() <= 0:
+            self.__gegner.set_dead()
+            self.__punkte += 1
+            self.__gegner = Gegner()
+            print("Gegner besiegt")
+        else:
+            self.__spieler.set_hp(self.__spieler.get_hp() - self.__gegner.get_atk())
+            if self.__spieler.get_hp() <= 0:
+                print("Spieler besiegt")
+                self.stop()
 
     def spielzug(self):
-        self.spieler.ziehe()
-        self.gegner.ziehe()
-        self.zeit.tick()
+        self.__spieler.ziehe()
+        self.__gegner.ziehe()
 
     def spielende(self):
-        self.zeit.stop()
         self.spielstand()
         print("Spielende")
 
-    def start(self):
-        while self.zeit.get_time() > 0:
-            self.spielzug()
+    def stop(self):
         self.spielende()
